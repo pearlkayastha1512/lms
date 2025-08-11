a4e9754bcdb04212ac25d696e5edf5eeb5fd12f9
pearlkayastha1512 <167806394+pearlkayastha1512@users.noreply.github.com>
2025-08-11 15:45:25 +0530

    stripe added

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,4 +1,7 @@
+import Course from "../Models/Course.js";
+import Purchase from "../Models/Purchase.js";
 import User from "../Models/User.js";
+import Stripe from "stripe";
 
 
 //get user data
@@ -25,6 +28,57 @@ export const userEnrolledCourses = async(req,res)=>{
         const userId = req.auth().userId;
         const userData = await User.findById(userId).populate('enrolledCourses')
         res.json({success:true,enrolledCourses: userData.enrolledCourses})
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+//Purchase Course
+export const purchaseCourse = async(req,res)=>{
+    try {
+        const {courseId} = req.body
+        const {origin} = req.headers
+        const userId = req.auth().userId
+        const userData = await User.findById(userId)
+        const courseData = await Course.findById(courseId)
+        if(!userId || !courseData){
+            return res.json({success:false,message:'Data Not Found'})
+        }
+        const purchaseData = {
+            courseId: courseData._id,
+            userId,
+            amount: (courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2),
+        }
+
+        const newPurchase = await Purchase.create(purchaseData)
+
+        //Stripe Gateway Initilaize
+        const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+        const currency = process.env.CURRENCY.toLowerCase()
+
+        //creating line items for stripe
+        const line_items = [{
+            price_data:{
+                currency,
+                product_data:{
+                    name: courseData.courseTitle
+                },
+                unit_amount: Math.floor(newPurchase.amount) * 100
+            },
+            quantity: 1
+        }]
+
+        const session = await stripeInstance.checkout.sessions.create({
+            success_url: `${origin}/loading/my-enrollments`,
+            cancel_url:`${origin}/`,
+            line_items: line_items,
+            mode: 'payment',
+            metadata:{
+                puchaseId: newPurchase._id.toString()
+            }
+        })
+        res.json({sucesss:true,session_url: session.url})
     } catch (error) {
         res.json({success:false,message:error.message})
     }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './Configs/mongodb.js'
-import { clerkWebhooks } from './Controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './Controllers/webhooks.js'
 import educatorRouter from './Routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './Configs/cloudinary.js'
@@ -32,6 +32,7 @@ app.get('/', (req,res)=> res.send("API working"))
 app.use('/api/educator',express.json(),educatorRouter)
 app.use('/api/course',express.json(),courseRouter)
 app.use('/api/user',express.json(),userRouter)
+app.post('/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 
 
 //PORT 
